c7dbb507e63a75c7a96c5b4c1201425b85a13cef
Tobias Megies <megies@geophysik.uni-muenchen.de>
2023-06-27 17:25:30 +0200

    django update: try to adjust to middleware changes
    see https://docs.djangoproject.com/en/4.2/topics/http/middleware/#upgrading-pre-django-1-10-style-middleware

--- src/jane/settings.py
+++ src/jane/settings.py
@@ -83,7 +83,7 @@ TEMPLATES = [{
 }]
 
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.gzip.GZipMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
